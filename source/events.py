de483c1c4e41c113ec674346e412d6154185660b
TheEnemyOfSleep <36903149+TheEnemyOfSpeel@users.noreply.github.com>
2019-01-26 06:54:37 +0300

    Translate GUI from tkinter to PyQt

--- source/events.py
+++ source/events.py
@@ -1,40 +1,10 @@
-from tkinter import messagebox, ttk
+from PyQt5.QtWidgets import qApp
 
 
 class MenuBarEvents(object):
 
-    def __init__(self, event=None, master=None):
-        self.event = event
+    def __init__(self, master=None):
         self.master = master
 
-    # If one cascade button in the focus, another csd buttons automatly get focused too(when mouse hover their)
-    def mouse_check_btn(self, csd_btn):
-        if str(csd_btn.focus_get()).find("!menu.") != -1:
-           self.event.widget.focus()
-
-    # Remove focus from cascade button when it focused and click
-    def unfocus_csd_btn(self, csd_btn):
-        if str(csd_btn.focus_get()) == str(self.event.widget) and str(csd_btn.focus_get()).find("!menu") != -1:
-            self.event.widget.configure(takefocus=0)
-            self.master.focus_force()
-        else:
-            self.event.widget.configure(takefocus=1)
-
-    # If entered on the rmf button, then add right menu frame
-    def forget_all_rmf(self, all_rmf, lvl=None, button=None):
-        for rmf in all_rmf:
-            # Check lvl rmf for forgetting place
-            if lvl+1 is rmf.lvl:
-                rmf.place_forget()
-
-    def entered_buttons_mf(self, menu_frame=None, all_rmf=None, lvl=None):
-        self.forget_all_rmf(all_rmf, lvl, self.event.widget)
-        if lvl is 1:
-            menu_frame.place(x=self.event.widget.winfo_x() + self.event.widget.winfo_width() * 1.95,
-                             y=self.event.widget.winfo_y() + self.event.widget.winfo_height() * 3)
-        else:
-            menu_frame.place(x=self.event.widget.winfo_x() + self.event.widget.winfo_width(),
-                       y=self.event.widget.winfo_y() + self.event.widget.winfo_height())
-
-    def test(self):
-        print("Noice")
+    def quit_trigger(self):
+        qApp.quit()
--- source/style.py
+++ source/style.py
@@ -1,6 +1,7 @@
 from tkinter import ttk, PhotoImage
+from tkinter.font import Font
 
-# Theme style class for change interface
+# Theme bluecyan class for change interface
 
 
 class Theme(ttk.Style):
@@ -9,19 +10,20 @@ class Theme(ttk.Style):
         super(Theme, self).__init__()
         self.style_dict = {'relief': "flat", 'bg_window': "#202533", 'bg_frame': "#181c26",
                            'fg_frame': "#a4a4a5", 'bg_btn_def': "#c8c8c846", 'bg_btn_point': "#ffffff46",
-                           'bg_btn_sel': "#32323246", 'bg_separator': "#181b26"}
+                           'bg_btn_sel': "#32323246", 'bg_separator': "#181b26", 'font_family': "Sans Sherif",
+                           'font_size': 8, 'font_weight': "bold", 'selected_bg': "#16a4b5"}
 
     def menu_separate_style(self, background="#181b26"):
         self.configure("Menu.TSeparator", background=background)
 
     def menu_btn_style(self, foreground="white", background="#202533", relief="flat",
-                       focus_background="#16a4b5"):
+                       focus_background="#16a4b5", font_family="Sans Sherif", font_size=8, font_weight="bold"):
 
         # Set flat theme "clam"
         self.theme_use("clam")
 
         # set configuration for button
-        self.configure("Menu.TButton", foreground=foreground, relief=relief, font=("Sans Sherif", 8, "bold"),
+        self.configure("Menu.TButton", foreground=foreground, relief=relief, font=(font_family, font_size, font_weight),
                              background=background, width=0, height=0)
 
         # remove focus ring for button
@@ -38,6 +40,42 @@ class Theme(ttk.Style):
 
         # set background focus color for button
         self.map("Menu.TButton", background=[('focus', focus_background)])
+
+    def menu_frame_main_btn_style(self, foreground="white", background="#202533", relief="flat",
+                                  focus_background="#16a4b5"):
+
+        # Set flat theme "clam"
+        self.theme_use("clam")
+
+        # set configuration for frame_mainbtns
+        self.configure("MenuFrame.TLabel", foreground=foreground, relief=relief, font=("Sans Sherif", 8, "bold"),
+                       background=background, anchor='w')
+
+        self.configure("MenuFrame.TFrame", foreground=foreground, relief=relief,
+                       background=background)
+
+        self.configure("ActiveMenuFrame.TLabel", foreground=foreground, relief=relief, font=("Sans Sherif", 8, "bold"), background=focus_background)
+
+        self.configure("ActiveMenuFrame.TFrame", foreground=foreground, relief=relief, background=focus_background)
+
+        self.layout("MenuFrame.TLabel", [
+            ("Label.focus", None), # Del focus ring
+            ("Label.background", {"children": # get children background
+                                                       [("Label.padding", {"children": # get children padding
+                                                                            [("Label.label", {"side": "left", "expand": 1})]
+                                                                            })]
+                                   })
+        ])
+
+        self.layout("ActiveMenuFrame.TLabel", [
+            ("Label.focus", None), # Del focus ring
+            ("Label.background", {"children": # get children background
+                                                       [("Label.padding", {"children": # get children padding
+                                                                            [("Label.label", {"side": "left", "expand": 1})]
+                                                                            })]
+                                   })
+        ])
+
 
     def menu_frame_btn_style(self, foreground="white", background="#202533", relief="flat", focus_background="#16a4b5"):
 
